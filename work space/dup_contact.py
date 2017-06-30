893783668c865df4fdae65395f3ed9c30042f204
Super <675153178@qq.com>
2017-06-30 13:36:35 +0800

    dup_contact upload

--- work space/dup_contact.py
+++ work space/dup_contact.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 # author:Super
+# python3版本
 import os,openpyxl
 os.chdir('D:\zlianxi\dup_contact')
 wbf = openpyxl.load_workbook('data.xlsx')
 sheet=wbf.active
 hang = sheet.max_row + 1
 lie = sheet.max_column-2
-print hang,lie
+print (hang,lie)
 list1=['C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X']
 for i in range(2,hang):
     d = ''
     for y in range(lie):
         a=sheet[list1[y]+'1'].value
         b=sheet[list1[y]+str(i)].value
-        if b==None:
-            b=''
-        c = a+':'+b+';'
-        d=d+' '+c
-    sheet['B' + str(i)]=d
+        if b != None:
+            # b = ''
+            c = str(a) + ':' + str(b) + ';'
+            d = d+' '+c
+    sheet['B' + str(i)] = d
 wbf.save('data.xlsx')
