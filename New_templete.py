5ee9923eae9a9e3ee9c0560ef041398cb2f222f0
Super <675153178@qq.com>
2017-08-03 17:13:36 +0800

    New templete version1

--- New_templete.py
+++ New_templete.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # author:Super
-# 该代码需要从第三列开始抓取
+# 该代码需要从第三列开始放入
 import os,openpyxl,xlrd
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font
@@ -29,8 +29,8 @@ for row in range(2, hang3):
 
 spam2 = {}
 for row in range(2, hang4):
-    flag = sheetcity1['A' + str(row)].value.lower()
-    number = sheetcity1['B' + str(row)].value
+    flag = sheetcity2['A' + str(row)].value.lower()
+    number = sheetcity2['B' + str(row)].value
     spam2.setdefault(flag, number)
 
 os.chdir('D:\zlianxi\New_templete\Collect_data')
@@ -50,7 +50,6 @@ for foldername,subfolder,excels in os.walk(filepath):
     choose=raw_input('请输入你的模板：1-response；2-NGCC；3-leads \n')
     if choose=='1':
         for k in range(1, lie):
-
             liebiao = get_column_letter(k)
             if sheet1[liebiao + '1'].value.lower() in spam.keys():
                 kk = get_column_letter(spam[sheet1[liebiao + '1'].value.lower()])
@@ -80,7 +79,57 @@ for foldername,subfolder,excels in os.walk(filepath):
                     sheet[kk + str(j)] = sheet1[liebiao + str(j)].value
                     j += 1
     else:
+        print '-' * 50
         print '请输入 1 or 2 or 3'
+        print '重新运行吧：ctrl & alt + F10'
+        print '-' * 50
+
+from datetime import *
+import time
+time2=time.strftime('%d-%b-%Y',time.localtime())
+time1=datetime.today()
+hang2 = sheet.max_row + 1
+if choose=='1':
+    for i in range(2,hang2):
+        sheet['D' + str(i)] = 'Others'
+        sheet['E' + str(i)] = 'Data Cleansing'
+        sheet['J' + str(i)] = str(time2)
+        if sheet['U' + str(i)] == 'Hong Kong':
+            sheet['S' + str(i)]='HONG KONG'
+            sheet['T' + str(i)]='Hong Kong'
+        else:
+            sheet['S' + str(i)] = 'TAIWAN'
+            sheet['T' + str(i)] = '台湾'
+elif choose=='2':
+    for i in range(2,hang2):
+        sheet['D' + str(i)] = 'Call Center'
+        sheet['E' + str(i)] = 'Called'
+        sheet['J' + str(i)] = str(time2)
+        if sheet['AL' + str(i)].value!=None:
+            sheet['AL' + str(i)] = 'Partner_Led_Customer:' + sheet['AL' + str(i)].value
+        else:
+            sheet['AL' + str(i)] = 'Partner_Led_Customer:'
+        if sheet['U' + str(i)] == 'Hong Kong':
+            sheet['S' + str(i)]='HONG KONG'
+            sheet['T' + str(i)]='Hong Kong'
+        else:
+            sheet['S' + str(i)] = 'TAIWAN'
+            sheet['T' + str(i)] = '台湾'
+elif choose=='3':
+    for i in range(2,hang2):
+        sheet['D' + str(i)] = 'Live Event'
+        # sheet['E' + str(i)] = 'Feedback Survey'
+        sheet['J' + str(i)] = str(time2)
+        sheet['AL' + str(i)] = 'BANT'
+        sheet['AN' + str(i)] = 'YES'
+        sheet['AO' + str(i)] = 'YES'
+        sheet['AP' + str(i)] = 'YES'
+        if sheet['U' + str(i)] == 'Hong Kong':
+            sheet['S' + str(i)]='HONG KONG'
+            sheet['T' + str(i)]='Hong Kong'
+        else:
+            sheet['S' + str(i)] = 'TAIWAN'
+            sheet['T' + str(i)] = '台湾'
 
 ft = Font(name='Arial', size=12, bold=True)
 ft1 = Font(name='Arial', size=12, bold=True, color=RED)
--- text.py
+++ text.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 # author:Super
-# from datetime import *
-# import time
-# time2=time.strftime('%b-%Y',time.localtime())
-# time1=datetime.today()
-# print time1.day
-# print time2
+from datetime import *
+import time
+time2=time.strftime('%d-%b-%Y',time.localtime())
+time1=datetime.today()
+print time1.day
+print time2
 
-a={'b':123}
