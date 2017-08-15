42f999482de31990ee019ee688a2c82612f39578
Super <675153178@qq.com>
2017-08-15 17:52:06 +0800

    new templete clean

--- New_templete_clean.py
+++ New_templete_clean.py
@@ -10,6 +10,9 @@ file = 'Clean_data.xlsx'
 if os.path.exists(file):
     os.remove(file)
 filepath = unicode('D:\zlianxi\New_templete_clean', 'utf-8')
+list1=['Segment']
+list2=['AM']
+
 for foldername,subfolder,excels in os.walk(filepath):
     Clean_data = openpyxl.Workbook()
     sheet = Clean_data.create_sheet(index=0, title='data')
@@ -18,17 +21,41 @@ for foldername,subfolder,excels in os.walk(filepath):
     hang = sheet1.max_row+1
     lie = sheet1.max_column+1
     print '数据量统计：%s'%(hang-2)
+    i=0
     for k in range(1, lie):
         liebiao = get_column_letter(k)
-        sheet[liebiao + '1'] = sheet1[liebiao + '1'].value
-        for j in range(2, hang):
-            if sheet1[liebiao + '1'].value=='COMPANY_NAME' or \
-                sheet1[liebiao + '1'].value == 'LAST_NAME':
-                sheet[liebiao + str(j)] = sheet1[liebiao + str(j)].value.strip()
+        liebiao1=get_column_letter(k+i)
+        i+=1
+        for j in range(1, hang):
+            sheet[liebiao1 + str(j)] = sheet1[liebiao + str(j)].value
 
-            else:
-                sheet[liebiao + str(j)]=sheet1[liebiao + str(j)].value
+    hang1 = sheet.max_row + 1
+    lie1 = sheet.max_column + 1
+    for kk in range(1 , lie1):
+        lb=get_column_letter(kk)
+        lb_1=get_column_letter(kk+1)
+        for jj in range(2,hang1):
+            if sheet[lb + '1'].value in list1:
+                sheet[lb_1 + '1'] = '标准segment'
+                sheet[lb_1 + str(jj)] = segment.get(sheet[lb + str(jj)].value)
+            if sheet[lb + '1'].value in list2:
+                sheet[lb_1 + '1'] = '标准AM'
+                if sheet[lb + str(jj)].value!=None :
+                    if sheet[lb + str(jj)].value in AM:
+                        sheet[lb_1 + str(jj)] = sheet[lb + str(jj)].value
+                    else:
+                        sheet[lb_1 + str(jj)] ='XXXX'
 
+
+
+        # sheet[liebiao1 + '1'] = sheet1[liebiao + '1'].value
+        # for j in range(2, hang):
+        #     if sheet1[liebiao + '1'].value=='COMPANY_NAME' or \
+        #         sheet1[liebiao + '1'].value == 'LAST_NAME':
+        #         sheet[liebiao + str(j)] = sheet1[liebiao + str(j)].value.strip()
+        #
+        #     else:
+        #         sheet[liebiao + str(j)]=sheet1[liebiao + str(j)].value
         # if sheet1[liebiao + '1'].value.lower() in spam.keys():
         #     kk = get_column_letter(spam[sheet1[liebiao + '1'].value.lower()])
         #     sheet[kk + '1'] = sheet1[liebiao + '1'].value
--- TTT.py
+++ TTT.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
 # author:Super
-spam={'a':1,'b':2}
+segment={'PL-NN':'Partner_Led_Customer:',\
+         'SP-CHT':'Service_Provider:',\
+         'COMM NAMED-TAM':'Partner_Led_Customer:',\
+         'COMM NAMED-VSAM':'Partner_Led_Customer:',\
+         'ENT-FSI':'ENT:',\
+         'ENT':'ENT:',\
+         'PS':'Public Sector:',\
+         'COM-N':'Partner_Led_Customer:',\
+         'COM-NN':'Partner_Led_Customer:',\
+         'SP':'Service_Provider:'}
+AM=['tiflee', 'fredyche', 'ticheng2', 'stanleyw', 'janchian',\
+    'alexhuan', 'autsai', 'mingcche', 'tommli', 'janetyua',\
+    'jaslu', 'jeffrlin',	'eritsai',	'jimhung', 'lwenchi',\
+    'resun', 'yuanwang',	'wilslin', 'cindhuan', 'fenwu']
