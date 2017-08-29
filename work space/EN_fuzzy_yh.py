17564f78b684a8c1d9d6c71eaeb1e6636f657261
Super <675153178@qq.com>
2017-08-29 10:02:40 +0800

    20170829 fuzzy

--- work space/EN_fuzzy_yh.py
+++ work space/EN_fuzzy_yh.py
@@ -29,7 +29,7 @@ for i in range(2,hang):
         kb = sheet1['E' + str(i)].value.replace('  ', ' ').replace('  ', ' ').split(' ')
         if kb[0] in citydata.keys():
             sheet1['E' + str(i)] = ' '.join(kb[1:])
-            sheet1['H' + str(i)] =citydata[kb[0]]
+            sheet1['I' + str(i)] =citydata[kb[0]]
         else:
             sheet1['E' + str(i)] = ' '.join(kb)
     if sheet1['C' + str(i)].value != None:
@@ -37,34 +37,38 @@ for i in range(2,hang):
         kd=sheet1['F'+str(i)].value.replace('  ',' ').replace('  ',' ').split(' ')
         if kd[0] in citydata.keys():
             sheet1['F' + str(i)] = ' '.join(kd[1:])
-            sheet1['I' + str(i)] = citydata[kd[0]]
+            sheet1['J' + str(i)] = citydata[kd[0]]
         else:
             sheet1['F' + str(i)] = ' '.join(kd)
 for i in range(2,hang):
     ee=sheet1['E' + str(i)].value
-    ff=sheet1['E' + str(i)].value
+    ff=sheet1['F' + str(i)].value
     if ee != None and ff != None:
         ee1=ee.split()
         ff1=ff.split()
-        if  len(ff1)>=2 :
-            if ee1[0]!=ff1[0] or ee1[0]!=ff1[1]:
-                sheet1['G'+str(i)]='关键字不相等-high'
-        elif len(ee1)>=2 :
-            if ff1[0]!=ee1[0] or ff1[0]!=ee1[1]:
-                sheet1['G' + str(i)] = '关键字不相等-high'
+        if ee1[0]==ff1[0] :
+            sheet1['G'+str(i)]='关键字相等'
+        else:
+            sheet1['G' + str(i)] = '不相等-需要核对'
+            if len(ee1)>=2 and ee1[1]==ff1[0]:
+                sheet1['H' + str(i)] = '2 -- 1 相等'
+            elif len(ff1)>=2 and ff1[1]==ee1[0]:
+                sheet1['H' + str(i)] = '1 -- 2 相等'
+            elif  ee1[0] in ff1[0] or ff1[0] in ee1[0]:
+                sheet1['H' + str(i)] = '包含关系'
 
-
-
-
-
+    else:
+        sheet1['G' + str(i)] = '不匹配'
 
 sheet1.freeze_panes='A2'
 sheet1['E1']='OR_NAME'
 sheet1['F1']='MAP_NAME'
 sheet1['G1']='预判断'
 sheet1['G1'].font = ft3
-sheet1['H1']='OR_城市'
-sheet1['H1'].font=ft2
-sheet1['I1']='MAP_城市'
+sheet1['H1']='预判断2'
+sheet1['H1'].font=ft1
+sheet1['I1']='or_城市'
 sheet1['I1'].font=ft2
+sheet1['J1']='MAP_城市'
+sheet1['J1'].font=ft2
 or_wb.save('data_fuzzy.xlsx')
