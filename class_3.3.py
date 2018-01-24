a07cce8a5c7fea6153cb1aab685e0e9253be0ce1
Super <675153178@qq.com>
2018-01-24 17:17:51 +0800

    pandas 4

--- class_3.3.py
+++ class_3.3.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# import pandas as pd
-# pd.set_option('expand_frame_repr',False)
-# # pd.set_option('max_colwidth',10)
-# df=pd.read_csv('sz000002.csv',
-#                # nrows=15,
-#                skiprows=1,
-#                usecols=['股票代码','交易日期','收盘价','成交量','MACD_金叉死叉'],
-#                error_bad_lines=False,
-#                na_filter='NULL')
+import pandas as pd
+pd.set_option('expand_frame_repr',False)
+# pd.set_option('max_colwidth',10)
+df=pd.read_csv('sz000002.csv',
+               nrows=10,
+               skiprows=1,
+               usecols=['交易日期','股票代码','收盘价','成交量','MACD_金叉死叉'],
+               error_bad_lines=False,
+               na_filter='NULL')
 
 # print df['收盘价']==22.29
 #
@@ -52,3 +52,45 @@
 # )
 #
 # print df3
+
+# print df.rename(columns={'股票代码':'股票', 'MACD_金叉死叉':'金叉死叉'})
+
+# print df.empty
+# print pd.DataFrame().empty
+
+# print df.T
+
+# print df['股票代码'].str[:2]
+
+# print df['股票代码'].str.upper()
+# print df['股票代码'].str.lower()
+# print df['股票代码'].str.len()
+# print df['股票代码'].str.strip()
+# print df['股票代码'].str.contain()
+# print df['股票代码'].str.replace('sz','sh')
+
+# print df['新浪概念'].str.split('；',expand=True)
+
+df['交易日期']=pd.to_datetime(df['交易日期'])
+
+# print df['交易日期']
+# print df['交易日期'].dt.year
+# print df['交易日期'].dt.dayofyear
+# print df['交易日期'].dt.weekday
+# print df['交易日期'].dt.weekday_name
+# print df['交易日期'].dt.days_in_month
+# print df['交易日期'].dt.is_month_end
+
+# print df['交易日期']+pd.Timedelta(days=1)
+
+# print df['交易日期']+pd.Timedelta(days=1)-df['交易日期']
+
+# df['收盘价_3_mean']=df['收盘价'].rolling(3).mean()
+#
+# print df[['收盘价','收盘价_3_mean']]
+
+df['收盘价_now']=df['收盘价'].expanding().mean()
+
+# print df[['收盘价','收盘价_now']]
+
+# df.to_csv('output.csv', encoding='gbk', index=False)
