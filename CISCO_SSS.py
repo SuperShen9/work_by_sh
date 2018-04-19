ec870729a0553ac60a81195a3cfd191bced9fde5
Super <675153178@qq.com>
2018-04-19 17:35:57 +0800

    20180419

--- CISCO_SSS.py
+++ CISCO_SSS.py
@@ -16,6 +16,8 @@ df = df[df['Program Event Code'].notnull()]
 df = df[df['Lead ID'] != '00Q340000220oWL']
 df = df[df['Lead ID'] != '00Q34000021NrfJ']
 df = df[df['Lead ID'] != '00Q34000021zgeF']
+df = df[df['Lead ID'] != '00Q34000022TK55']
+df = df[df['Lead ID'] != '00Q34000022TK4v']
 
 # 导入 “活动名称” 和 “修改名称” 的2个表
 df_event = pd.read_excel('sss_event.xlsx')
