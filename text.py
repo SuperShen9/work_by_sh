e59bfb30f5b4513eaa71e0d031a5e39f20537d9b
Super <675153178@qq.com>
2018-05-07 10:33:11 +0800

    20180507

--- text.py
+++ text.py
@@ -12,23 +12,23 @@ os.chdir('C:\\Users\Administrator\Desktop')
 # exit()
 
 # # 查看text文件
-file=open('text.txt')
+file=open('1.txt')
 lines=file.readlines()
 print lines
 # for i in lines:
 #     print i
 # exit()
 
-df=pd.read_excel('sheet1.xlsx')
-
-
-for i in range(1,df.shape[0]):
-    key = 'name'
-    val = df['NAME'].loc[i].encode('gbk')
-    # print val
-    fl = open('RUN.txt', 'a')
-    fl.write('\t\r"name\r"' + ':' +'\r"' + val+'\r"'+',')
-    fl.write("\n")
+# df=pd.read_excel('sheet1.xlsx')
+#
+#
+# for i in range(1,df.shape[0]):
+#     key = 'name'
+#     val = df['NAME'].loc[i].encode('gbk')
+#     # print val
+#     fl = open('RUN.txt', 'a')
+#     fl.write('\t\r"name\r"' + ':' +'\r"' + val+'\r"'+',')
+#     fl.write("\n")
 
 
 
--- zhuan_weiyi_run.py
+++ zhuan_weiyi_run.py
@@ -24,20 +24,15 @@ df=pd.read_excel('sheet1.xlsx')
 if os.path.exists('RUN'):
     shutil.rmtree('RUN')
 os.makedirs('C:\\Users\Administrator\Desktop\\RUN')
-
 os.chdir('C:\\Users\Administrator\Desktop\\RUN')
-
-# print df.columns[:-4]
-# print df.columns[-4:]
-# exit()
+# df.shape[0]
 
 for i in range(df.shape[0]):
     count=0
     for x in df.columns:
         count+=1
         val = df[x].loc[i]
-        # if x == 'docUrl' or x == 'remark':
-        #     val = val
+
         if isinstance(val, float):
             val = ''
         else:
@@ -47,30 +42,30 @@ for i in range(df.shape[0]):
             if x == 'webName':
                 fl.write('{\n')
                 fl.write('\t\r"webUrl": "http://www.chictr.org.cn/searchproj.aspx",\n')
-                fl.write('\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + ',')
+                fl.write('\t\r"{}": "{}",'.format(x, str(val)))
                 fl.write("\n")
             elif x == 'remark':
                 fl.write('\t\r"remark":"",\n')
             else:
-                fl.write('\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + ',')
+                fl.write('\t\r"{}": "{}",'.format(x, str(val)))
                 fl.write("\n")
         elif count==len(df.columns)-3:
             fl.write('\t\r"info": {\n')
-            fl.write('\t\t\r"name\r"' + ':' + '\r"' + str(val) + '\r"' + ',\n')
+            fl.write('\t\t\r"name": "{}",\n'.format(str(val)))
 
         elif count==len(df.columns)-1:
             if val[-1]=='0':
-                fl.write('\t\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val)[:4] + '\r"' + ',\n')
+                fl.write('\t\t\r"{}": "{}",\n'.format(x, str(val)[:4]))
             else:
-                fl.write('\t\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val)[4:] + '\r"' + ',\n')
+                fl.write('\t\t\r"{}": "{}",\n'.format(x, str(val)[4:]))
 
-        elif count == len(df.columns) :
-            fl.write('\t\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + '\n')
+        elif count == len(df.columns):
+            fl.write('\t\t\r"{}": "{}"\n'.format(x, str(val)))
             fl.write('\t}\n')
             fl.write('}')
         else:
-            fl.write('\t\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + ',')
-            fl.write("\n")
+            fl.write('\t\t\r"{}": "{}",\n'.format(x, str(val)))
+
 
 
 
