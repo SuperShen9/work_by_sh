5d98862b667a1dbe89151fcb134b31a59c42edc3
Super <675153178@qq.com>
2018-05-07 14:59:07 +0800

    20180507

--- zhuan_Edu_run.py
+++ zhuan_Edu_run.py
@@ -16,7 +16,7 @@ os.makedirs('C:\\Users\Administrator\Desktop\\RUN')
 
 os.chdir('C:\\Users\Administrator\Desktop\\RUN')
 
-count_all = 0
+
 for i in range(df.shape[0]):
     fl = open('%s-%s-%s.txt' % (df['name'].loc[i], df['organization'].loc[i], df['webName'].loc[i]), 'a')
     for x in df.columns:
@@ -43,22 +43,29 @@ for i in range(df.shape[0]):
     # exit()
     # print df_ren.groupby('year').first().reset_index()
     # exit()
-
+    # print len(df_ren)
+    count_all = 0
     for y in df_ren.groupby('year').first().reset_index()['year']:
         df_year=df_ren[df_ren['year']==y]
         fl.write('\t\t\t\r"year": "{}",'.format(y) + '\n')
         fl.write('\t\t\t\r"info": [{' + '\n')
 
         count=0
+
         all=len(df_year.groupby('unitName').first().reset_index()['unitName'])
         for n in df_year.groupby('unitName').first().reset_index()['unitName']:
             count+=1
+
+            # print count_all
             df_name = df_year[df_ren['unitName'] == n]
             df_name = df_name.reset_index(drop=True)
 
             fl.write('\t\t\t\t\r"unitName": "{}",'.format(n.encode('gbk')) + ',\n')
             fl.write('\t\t\t\t\r"unitInfo": [{\n')
             for ii in range(df_name.shape[0]):
+                #原来底层循环在这
+                count_all += 1
+
                 for xx in df_name.columns:
                     val_edu = df_name[xx].loc[ii]
                     if xx == 'holdingPeriod' or xx == 'creditHour' or xx == 'year' :
@@ -81,13 +88,11 @@ for i in range(df.shape[0]):
                         else:
                             fl.write('\t\t\t\t\t\t\r"{}": "{}",\n'.format(xx, str(val_edu)))
 
-            if count!=all:
-                fl.write("\t\t\t\t\t}\n\t\t\t\t]\n\n\t\t\t}, {\n")
-
-            # elif count_all==len(df_edu[df_edu['uuid'] == uuid]):
-            #     fl.write("\t\t\t\t\t}\n\t\t\t\t]\n\n\t\t\t}]\n\t\t\r}\n\t\r]\n\r]")
+            if count_all==len(df_ren):
+                fl.write("\t\t\t\t\t}\n\t\t\t\t]\n\n\t\t\t}]\n\t\t\r}\n\t\r]\n}")
             else:
                 fl.write("\t\t\t\t\t}\n\t\t\t\t]\n\n\t\t\t}]\n\t\t\r},\n\t\t\r{\n")
+
 
 
 
